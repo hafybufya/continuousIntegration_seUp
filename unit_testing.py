b1cbeacf2c4422510f220dacb87c228fb5e7482c
Hafsa Abdi <126822951+hafybufya@users.noreply.github.com>
2025-09-29 12:26:33 +0100

    Create unit_testing.py
    Copying basic unittest into repository

---
+++ unit_testing.py
@@ -0,0 +1,24 @@
+import unittest
+from my_function_continuous import incrementing_function
+
+# define the unit tests
+class my_unit_tests(unittest.TestCase):
+
+       # test adding integers
+    def test_incrementing_pos_integer(self):     
+ 
+        self.assertEqual(incrementing_function(5), 6)
+
+        # test adding negative integers
+    def test_incrementing_neg_integer(self):
+
+        self.assertEqual(incrementing_function(-6), -5)
+
+        # test adding floats
+    def test_incrementing_with_float(self):
+
+        self.assertEqual(incrementing_function(3.1), 4.1)
+
+# run the tests
+if __name__ == "__main__":
+    unittest.main()
